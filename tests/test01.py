9ed01087e6acbb6399291d9b21c7c6925a28f344
Jeremy Ferguson <jmfergie@berkeley.edu>
2020-07-24 19:17:44 -0500

    Added more setup tests

--- tests/test01.py
+++ tests/test01.py
@@ -2,26 +2,42 @@ import unittest
 import pygrad
 
 class TestSetup(unittest.TestCase):
+    def testErrorFileSetup(self,fname,error):
+        with self.assertRaises(error):
+            pygrad.setup(fname)
+
+    def testErrorFileSetupFormat(self,fname):
+        self.testErrorFileSetup(fname, InfileFormatException)
+
     def testMissingLine(self):
-        with self.assertRaises(InfileFormatException):
-            pygrad.setup('error-ins/error-01.txt')
+        self.testErrorFileSetupFormat('error-ins/error-01.txt')
 
     def testMissingFile(self):
-        with self.assertRaises(FileNotFoundError):
-            pygrad.setup('error-ins/error-00.txt')
+        self.testErrorFileSetup('error-ins/error-00.txt', FileNotFoundError)
     
     def testBlankFile(self):
-        with self.assertRaises(InfileFormatException):
-            pygrad.setup('error-ins/error-02.txt')
+        self.testErrorFileSetupFormat('error-ins/error-02.txt')
 
     def testNoGas(self):
-        with self.assertRaises(InfileFormatException):
-            pygrad.setup('error-ins/error-03.txt')
+        self.testErrorFileSetupFormat('error-ins/error-03.txt')
 
     def testAbsZero(self):
-        with self.assertRaises(InfileFormatexception):
-            pygrad.setup('error-ins/error-04.txt')
+        self.testErrorFileSetupFormat('error-ins/error-04.txt')
 
+    def testZeroPressure(self):
+        self.testErrorFileSetupFormat('error-ins/error-05.txt')
+
+    def testTooFewParameters(self):
+        self.testErrorFileSetupFormat('error-ins/error-06.txt')
+
+    def testTooManyParameters(self):
+        self.testErrorFileSetupFormat('error-ins/error-07.txt')
+    
+    def testNgasBig(self):
+        self.testErrorFileSetupFormat('error-ins/error-08.txt')
+
+    def testNgasMismatch(self):
+        self.testErrorFileSetupFormat('error-ins/error-09.txt')
 if __name__ == '__main__':
     unittest.main()
 
