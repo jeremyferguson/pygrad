d1de9fed994fa8ae2ca3cfcf46308e3ef49982a5
Jeremy Ferguson <jfergpython@gmail.com>
2020-11-29 12:20:59 -0800

    temporarily pythonized mixer

--- Pygrad/Mixers.pyx
+++ Pygrad/Mixers.pyx
@@ -7,18 +7,19 @@ import cython
 import numpy as np
 cimport numpy as np
 
-@cython.cdivision(True)
-@cython.boundscheck(False)
-@cython.wraparound(False)
-@cython.nonecheck(False)
-cpdef Mixer(Pygrad object):
+#@cython.cdivision(True)
+#@cython.boundscheck(False)
+#@cython.wraparound(False)
+#@cython.nonecheck(False)
+def Mixer(Pygrad object):
     """Loads the initial gas values for the mixture, particularly the momentum cross sections and other related values."""
-    cdef double AttachmentCrossSection[6][4000], ElectronCharge, JHI, JLOW, EnergyHigh, F2, BP, EnergyLow,rGas1
-    cdef int iEnergy, GasIndex, iProcess, p, Sum, J, i, j, iIonization, JJ, IL, I
-    cdef Gas gasData
+ #   cdef double AttachmentCrossSection[6][4000], ElectronCharge, JHI, JLOW, EnergyHigh, F2, BP, EnergyLow,rGas1
+    rGas1 = 0.0
+  #  cdef int iEnergy, GasIndex, iProcess, p, Sum, J, i, j, iIonization, JJ, IL, I
+   # cdef Gas gasData
     print("mixer beginning")
     ElectronCharge = 1.60217656e-19
-    object.MixObject.InitWithInfo(object.GasIDs, object.InelasticCrossSectionPerGas, object.N_Inelastic,object.PenningFraction, object.E, object.SqrtEnergy, object.NumberOfGases, object.EnergySteps, object.Which_Angular_Model, object.ElectronEnergyStep, object.Max_Electron_Energy, object.ThermalEnergy, object.TemperatureCentigrade, object.Pressure_Torr, object.PIR2, object.RhydbergConst)
+    object.MixObject.InitWithInfo(object.GasIDs, object.InelasticCrossSectionPerGas, object.N_Inelastic,object.PenningFraction, object.E, object.SqrtEnergy, object.NumberOfGases, object.EnergySteps, object.WhichAngularModel, object.ElectronEnergyStep, object.Max_Electron_Energy, object.ThermalEnergy, object.TemperatureCentigrade, object.Pressure_Torr, object.PIR2, object.RhydbergConst)
     object.MixObject.Run()
     ang = Ang()
     print('before for loop')
